45879bd379af67810cb94ca4e021cb0b10af435d
Mason Phillips <mjphillips2000@gmail.com>
2020-10-06 20:11:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  yes = ["Yes","No"]
+  yes.push << "Hell Nah"
 end
 
 def add_element_to_start_of_array(array, element)
